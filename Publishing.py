acc868af2aab88291c13be507135bb7e2c0e4ec1
Marian <eng.marianmaksimos@gmail.com>
2021-11-16 15:24:45 -0500

    count char

--- Publishing.py
+++ Publishing.py
@@ -1,26 +1,38 @@
+from numpy import append
 from sphinx.util import requests
 
 
 class Publishing:
 
-    def __init__(self, name, host, udp_socket, tcp_socket) -> object:
+    def __init__(self, name):
         super().__init__('Publishing')
         self.name = name
-        self.host = host
-        self.udp_socket = udp_socket
-        self.tcp_socket = tcp_socket
 
 
-# open text file and count the number of characters
-with open("file_to_upload.txt", "rt") as a_file:
-    data = a_file.read()
-    number_of_characters = len(data)
-    print('Number of characters in text file :', number_of_characters)
+    # open text file and count the number of characters
+    with open("file_to_upload.txt", "rt") as a_file:
+        data = a_file.read()
+        number_of_characters = len(data)
+        print('Number of characters in text file :', number_of_characters)
+
+        if int(number_of_characters) <1:
+            print("file is empty")
+            exit
+        elif int(number_of_characters) >1:
+            if int(number_of_characters) <= 200:
+                print("file will be sent in 1 segment")
+                # sendfile
+            else:
+                print("file will be fragmented")
+                #call split
+                #call send
+
+
 
 
 
 # upload text file
 
-with open("file_to_upload.txt", "rt") as a_file:  # open(file_name, mode) , rt--> read text   as  target
-    file_dict = {"file_to_upload.txt": a_file}
-    response = requests.post("http://httpbin.org/post", files=file_dict)  # upload the file to the url
+# with open("file_to_upload.txt", "rt") as a_file:  # open(file_name, mode) , rt--> read text   as  target
+# file_dict = {"file_to_upload.txt": a_file}
+# response = requests.post("http://httpbin.org/post", files=file_dict)  # upload the file to the url
