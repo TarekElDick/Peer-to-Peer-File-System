1c7488e7f425dde7cac929d152c0449a7a818e45
Aida <kordi_aida43@yahoo.com>
2021-11-16 23:18:42 -0500

    test

---
+++ Client_Requests_Classes/unregister.py
@@ -0,0 +1,11 @@
+from Client_Requests_Classes import request
+
+
+class Unregister(request.clientRequest):
+    def __init__(self, name):
+        super().__init__('unregister')
+        self.name = name
+
+    def getHeader(self):
+        header_string = '\n[' + self.request_type + ' | ' + str(self.rid) + ' | ' + self.name + ']\n'
+        return header_string
