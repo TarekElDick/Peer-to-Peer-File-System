eeb5a84b3bf6421745dd5f9b458cb0b3fbe2156c
Tarek El Dick <tarekdeek17@gmail.com>
2021-11-21 19:57:12 -0500

    #21 Created dedicated port to send information per command and added logically TCP for our udp communication. List of ack in server code

--- server_multi_client.py
+++ server_multi_client.py
@@ -16,12 +16,8 @@ class serverMultiClient(server.UDPServer):
         super().__init__(host, port)
         self.socket_lock = threading.Lock()
         self.list_of_registered_clients = list()
-        self.list_of_tuple_of_files = None
         self.list_of_available_files = list()
-        # self.textfl = list()
-
-        self.textfl = ["LAPTOP-155P80FB", "10.0.0.116", "53548", ["TEXT1", "TEXT2", "text6"],
-                       "LAPTOP-155P80FB", "10.0.0.116", "5656", ["TEXT3", "TEXT4"]]
+        self.list_of_acknowledgements = list()
 
     # 2. handle_request() - Handle client's request and send back the response after acquiring lock
     def handle_request(self, client_data, client_address):
@@ -31,31 +27,45 @@ class serverMultiClient(server.UDPServer):
 
         # Find out what kind of object it is and send it to the designated function
         if isinstance(client_request, register.Register):
-            self.try_registering(client_request, client_address)
+            if not self.check_if_already_ack(client_request):
+                self.try_registering(client_request)
+            return
         elif isinstance(client_request, unregister.Unregister):
-            self.try_unregistering(client_request, client_address)
-        elif isinstance(client_request, retrieve.Retrieve):
-            self.try_retrieve_all(client_request, client_address)
+            if not self.check_if_already_ack(client_request):
+                self.try_unregistering(client_request)
         elif isinstance(client_request, update_contact.UpdateContact):
-            self.try_updatingContact(client_request, client_address)
+            self.try_updatingContact(client_request)
 
-    def try_registering(self, re_request, client_address):
+    def try_registering(self, re_request):
+        print(re_request.getHeader())
+        client_address = (re_request.host, re_request.udp_socket)
+
         # Check if the client is already registered, if not add the client name to the list of clients, if already registered then deny the request
-        if self.check_if_client(re_request):  # might be able to just send request.name.
+        if self.check_if_client(re_request):
             msg_to_client = '[REGISTER-DENIED' + ' | ' + str(re_request.rid) + ' | ' + 'Client already registered]'
             self.printwt(msg_to_client)
+
+            array_to_append = [re_request.name, re_request.rid, msg_to_client, client_address]
+            self.list_of_acknowledgements.append(array_to_append)
+
+            self.sock.sendto(msg_to_client.encode('utf-8'), client_address)
+            return
+        else:
+            # register the client and inform the client
+            msg_to_client = '[REGISTERED' + ' | ' + str(re_request.rid) + ']'
+            self.printwt(msg_to_client)
+
+            array_to_append = [re_request.name, re_request.rid, msg_to_client, client_address]
+            self.list_of_acknowledgements.append(array_to_append)
+
+            self.list_of_registered_clients.append(re_request)
             self.sock.sendto(msg_to_client.encode('utf-8'), client_address)
             return
 
-        # register the client and inform the client
-        msg_to_client = '[REGISTERED' + ' | ' + str(re_request.rid) + ']'
-        self.printwt(msg_to_client)
-
-        self.list_of_registered_clients.append(re_request)
-        self.sock.sendto(msg_to_client.encode('utf-8'), client_address)
-
-    def try_unregistering(self, de_request, client_address):
+    def try_unregistering(self, de_request):
+        print(de_request.getHeader())
         if self.check_if_client(de_request):
+            client_address = self.get_client_udp_address(de_request)
             # if the client is registered then unregister them
             for obj in self.list_of_registered_clients:
                 if isinstance(obj, register.Register):
@@ -64,69 +74,17 @@ class serverMultiClient(server.UDPServer):
                         self.list_of_registered_clients.remove(obj)
                         msg_to_client = '[DE-REGISTERED' + ' | ' + str(de_request.rid) + ']'
                         self.printwt(msg_to_client)
+
+                        array_to_append = [de_request.name, de_request.rid, msg_to_client, client_address]
+                        self.list_of_acknowledgements.append(array_to_append)
+
                         self.sock.sendto(msg_to_client.encode('utf-8'), client_address)
                         return
         self.printwt('Ignoring request, client not registered')
+        return
 
-    def try_retrieve_all(self, ra_request, client_address):
-        list_of_files = " "
-        msg_to_client = "RETRIEVED-ALL  |  " + str(ra_request.rid)
-        j = 0
-
-        #  if self.check_if_client(up_request):
-
-        #  for index=range(len(self.textfl)):
-        for i in range(len(self.textfl)):
-            i = j
-            if i == len(self.textfl):
-                break
-
-            self.printwt("iiiiii:   " + str(i))
-            clientname = self.textfl[i]
-            self.printwt("clientname  " + clientname)
-            ipaddr = self.textfl[i + 1]
-            self.printwt("IP :  " + str(ipaddr))
-            udpport = self.textfl[i + 2]
-            self.printwt("port: " + str(udpport))
-            j = i + 3
-            self.printwt(j)
-            list_of_files = " "
-            length = len(self.textfl[i + 3])
-            self.printwt(str(length))
-            for files in range(len(self.textfl[i + 3])):
-                list_of_files = (list_of_files + " , " + self.textfl[i + 3][files])
-            #   self.printwt("listfiles : " + list_of_files)
-            else:
-                j = i + 4
-
-                self.printwt("listfiles : " + list_of_files)
-                self.printwt("jjjjjjjjjjjjjjjjj" + str(i))
-                self.printwt(clientname + "  " + ipaddr + " port :   " + \
-                             udpport + "list_of_files : " + list_of_files)
-
-                msg_to_client = (msg_to_client + '|' + \
-                                 (clientname) + ' | ' + ipaddr + ' | ' + udpport + '|' + \
-                                 list_of_files + ']')
-
-                self.printwt("end of client lists")
-
-        #          filename = self.textfl.values[i][0]
-        #          self.printwt(filename)
-        #          for peer in self.textfl[i][1]:
-        #           header = ' %s %s %s %s\n' % ( filename, peer[0], peer[1] )
-        #  self.printwt(filename)
-        #  self.printwt(peer[0])
-
-        self.printwt(msg_to_client)
-        self.sock.sendto(msg_to_client.encode('utf-8'), client_address)
-
-    # else:
-    #     msg_to_client = '[RETRIEVE-ERROR' + ' | ' + str(up_request.rid) + ' | ' + 'non-registered user]'
-    #     self.printwt(msg_to_client)
-    #     self.sock.sendto(msg_to_client.encode('utf-8'), client_address)
-    #     return
-
-    def try_updatingContact(self, up_request, client_address):
+    def try_updatingContact(self, up_request):
+        client_address = self.get_client_udp_address(up_request)
         if self.check_if_client(up_request):
             # if the client is registered then we can update the register object
             for obj in self.list_of_registered_clients:
@@ -140,21 +98,21 @@ class serverMultiClient(server.UDPServer):
                             up_request.name) + ' | ' + str(up_request.host) + ' | ' + str(
                             up_request.udp_socket) + ' | ' + str(up_request.tcp_socket) + ']'
                         self.printwt(msg_to_client)
+
+                        array_to_append = [up_request.name, up_request.rid, msg_to_client, client_address]
+                        self.list_of_acknowledgements.append(array_to_append)
+
                         self.sock.sendto(msg_to_client.encode('utf-8'), client_address)
 
         else:
             msg_to_client = '[UPDATE-DENIED' + ' | ' + str(up_request.rid) + ' | ' + str(
                 up_request.name) + ' | ' + 'Name does not Exist]'
             self.printwt(msg_to_client)
-            self.sock.sendto(msg_to_client.encode('utf-8'), client_address)
 
-    # TODO 5.1.1 Handle request client requests.
-    # publish
-    # remove
-    # retrieve_all
-    # retrieve_info
-    # search_file
-    # download
+            array_to_append = [up_request.name, up_request.rid, msg_to_client, client_address]
+            self.list_of_acknowledgements.append(array_to_append)
+
+            self.sock.sendto(msg_to_client.encode('utf-8'), client_address)
 
     def check_if_client(self, client_request):
         for obj in self.list_of_registered_clients:
@@ -163,6 +121,22 @@ class serverMultiClient(server.UDPServer):
                 if obj.name == client_request.name:
                     return True
         return False
+
+    def check_if_already_ack(self, client_request):
+        for s_list in self.list_of_acknowledgements:
+            if s_list[0] == client_request.name and s_list[1] == client_request.rid:
+                print(client_request.getHeader())
+                self.printwt(f'Already received this request. Resending the reply : {client_request.name}')
+                self.sock.sendto(s_list[2].encode('utf-8'), s_list[3])
+                return True
+        return False
+
+    def get_client_udp_address(self, client_request):
+        for obj in self.list_of_registered_clients:
+            if isinstance(obj,
+                          register.Register):  # for checking if client they are all register objects but isinstance is important to allow us to call obj.name
+                if obj.name == client_request.name:
+                    return obj.host, obj.udp_socket
 
     # 3. wait_for_client() - Override the server's wait_for_client() method to handle multiple clients by using an
     # infinite loop
@@ -188,7 +162,6 @@ class serverMultiClient(server.UDPServer):
 
 # 4. main() - Driver code to test the program
 def main():
-
     udp_server_multi_client = serverMultiClient(socket.gethostbyname(socket.gethostname()), 3001)
 
     udp_server_multi_client.configure_server()
