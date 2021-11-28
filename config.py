64d4ce809df3a2f1ece9e76a9ea755b4db115b3c
Tarek El Dick <tarekdeek17@gmail.com>
2021-11-27 20:55:39 -0500

    Added some things for #30

--- config.py
+++ config.py
@@ -5,9 +5,3 @@ UDP_TIMEOUT = 5
 TCP_TIMEOUT = 5
 BUFFER_SIZE = 1024
 
-# UPDATE THE IPV4 ADDRESS OF THE SERVER
-# if running both server and client on same machine
-SERVER_ADDRESS = (socket.gethostbyname(socket.gethostname()), 3001)
-
-# if running on separate machines just put the server ip here
-# SERVER_ADDRESS = ('', 3001)
