d50ce75b597eccdbe5b60e2e0698afb564548575
Marian <eng.marianmaksimos@gmail.com>
2021-11-29 22:07:10 -0500

    redo changes

--- client.py
+++ client.py
@@ -148,7 +148,7 @@ class Client:
             choice = int(choice)
             if choice != 0:
                 self.list_of_available_files = [self.list_of_available_files[choice - 1]]
-                #
+                #hh
         else:
             choice = [int(x) for x in choice.split(",")]
             user_choices = []
